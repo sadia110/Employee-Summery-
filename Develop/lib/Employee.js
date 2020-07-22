f7cdd950823b59ae5d18fe0f49716247cd219625
Sadia Batool <sadia110@my.yorku.ca>
2020-07-22 11:47:41 -0400

    add employee functions

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,22 @@
 // TODO: Write code to define and export the Employee class
+class Employee{
+    constructor( name, id, email,role='Employeee'){
+        this.id =id;  
+        this.name= name;
+        this.email= email;
+        this.role=role; 
+    } 
+    getName () {
+        return this.name 
+    } 
+    getID() {
+        return this.id
+    } 
+    getEmail () {
+        return this.email 
+    } 
+    getRole () {
+        return this.role
+    }
+}  
+module.exports =Employee
